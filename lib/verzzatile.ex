b82b26f47e0f77d78c2798699d061130a95c6f28
Adam Schmideg <adamschmideg@users.noreply.github.com>
2024-03-27 21:41:48 +0100

    Initialize

---
+++ lib/verzzatile.ex
@@ -0,0 +1,18 @@
+defmodule Verzzatile do
+  @moduledoc """
+  Documentation for `Verzzatile`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Verzzatile.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/verzzatile_test.exs
@@ -0,0 +1,8 @@
+defmodule VerzzatileTest do
+  use ExUnit.Case
+  doctest Verzzatile
+
+  test "greets the world" do
+    assert Verzzatile.hello() == :world
+  end
+end
