b2ed2106bbf6cda6c486048b780be0b4d5e92b6f
Adam Schmideg <adamschmideg@users.noreply.github.com>
2024-03-28 17:46:22 +0100

    Add and get a cell works

--- lib/verzzatile.ex
+++ lib/verzzatile.ex
@@ -16,22 +16,58 @@ defmodule Verzzatile do
     end
 
     def get_id(%{id: id}), do: id
-
     def get_id(id), do: id
 
   end
 
+  defmodule ZZstructure do
+    defstruct id_to_cell: %{}
+  end
+
+  use GenServer
+
+  # Client API
+
+  def start_link do
+    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+  end
+
   @doc """
-  Wraps the value in a cell and returns the cell.
+  Wraps the value in a cell, stores it, and returns the cell.
   """
   def add(value) do
     cell = Cell.new(value)
+    GenServer.cast(__MODULE__, {:add, cell})
     cell
   end
 
   def get(cell_or_id) do
     id = Cell.get_id(cell_or_id)
-    {:ok, cell_or_id}
+    cell = GenServer.call(__MODULE__, {:get, id})
+    if cell == nil do
+      {:error, :not_found}
+    else
+      {:ok, cell}
+    end
+  end
+
+  # Server Callbacks
+
+  @impl true
+  def init(initial_state) do
+    {:ok, initial_state}
+  end
+
+  @impl true
+  def handle_cast({:add, cell}, state) do
+    updated_state = Map.put(state, cell.id, cell)
+    {:noreply, updated_state}
+  end
+
+  @impl true
+  def handle_call({:get, cell_id}, _from, state) do
+    cell = Map.get(state, cell_id, nil)
+    {:reply, cell, state}
   end
 
 end
--- test/verzzatile_test.exs
+++ test/verzzatile_test.exs
@@ -3,6 +3,7 @@ defmodule VerzzatileTest do
   doctest Verzzatile
 
   test "Gets a cell that was added" do
+    {:ok, _pid} = Verzzatile.start_link()
     cell = Verzzatile.add('value')
     assert {:ok, cell_got} = Verzzatile.get(cell)
     assert cell_got.value == 'value'
