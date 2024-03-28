add48897c0c248ef83309813aa9f61f6ab69a1e5
Adam Schmideg <adamschmideg@users.noreply.github.com>
2024-03-28 16:13:34 +0100

    Start test for adding a cell. Not storing it yet

--- lib/verzzatile.ex
+++ lib/verzzatile.ex
@@ -3,16 +3,34 @@ defmodule Verzzatile do
   Documentation for `Verzzatile`.
   """
 
-  @doc """
-  Hello world.
+  defmodule Cell do
+    defstruct id: nil, value: nil
 
-  ## Examples
+    def new(value) do
+      id = "#{:rand.uniform 1000_000_000}"
+      new_with_id(id, value)
+    end
 
-      iex> Verzzatile.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    def new_with_id(id, value) do
+      %Cell{id: id, value: value}
+    end
   end
+
+  @doc """
+  Wraps the value in a cell and returns the cell.
+  """
+  def add(value) do
+    cell = Cell.new(value)
+    cell
+  end
+
+  def get(cell_or_id) do
+    id = if is_map(cell_or_id) do
+           cell_or_id.id
+         else
+           cell_or_id
+         end
+    cell_or_id
+  end
+
 end
--- test/verzzatile_test.exs
+++ test/verzzatile_test.exs
@@ -2,7 +2,10 @@ defmodule VerzzatileTest do
   use ExUnit.Case
   doctest Verzzatile
 
-  test "greets the world" do
-    assert Verzzatile.hello() == :world
+  test "Gets a cell that was added" do
+    cell = Verzzatile.add('value')
+    cell_got = Verzzatile.get(cell)
+    assert !is_nil(cell_got)
+    assert cell_got.value == 'value'
   end
 end
