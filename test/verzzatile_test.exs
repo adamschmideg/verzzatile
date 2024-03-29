b77bc98ef3768ef6c019a0f6b70ca727710767cb
Adam Schmideg <adamschmideg@users.noreply.github.com>
2024-03-29 18:06:38 +0100

    Some nice tests

--- test/verzzatile_test.exs
+++ test/verzzatile_test.exs
@@ -20,4 +20,21 @@ defmodule VerzzatileTest do
     assert cell1 = Verzzatile.prev(cell2, :friend)
     assert is_nil Verzzatile.prev(cell1, :friend)
   end
+
+  test "Connected cells are not connected in other dimensions" do
+    {:ok, _pid} = Verzzatile.start_link()
+    cell1 = Verzzatile.add('value1')
+    cell2 = Verzzatile.add('value2')
+    assert :ok = Verzzatile.connect(cell1, cell2, :friend)
+    assert is_nil Verzzatile.next(cell1, :enemy)
+    assert is_nil Verzzatile.prev(cell2, :enemy)
+  end
+
+  test "Cell can connect it itself" do
+    {:ok, _pid} = Verzzatile.start_link()
+    cell = Verzzatile.add('value')
+    assert :ok = Verzzatile.connect(cell, cell, :friend)
+    assert cell = Verzzatile.next(cell, :friend)
+    assert cell = Verzzatile.prev(cell, :friend)
+  end
 end
